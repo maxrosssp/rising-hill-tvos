71768e3f94db90ef1c47cda18f0e622ac8d40645
maxrosssp <maxwellspiegelman@gmail.com>
2022-03-15 16:21:15 -0400

    Update AppDelegate.swift

--- RisingHill/AppDelegate.swift
+++ RisingHill/AppDelegate.swift
@@ -18,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     static let tvBaseURL = "http://localhost:9001"
     static let tvBootURL = "\(AppDelegate.tvBaseURL)/application.js"
     
+//     func executeRemoteMethod(_ methodName: String, completion: @escaping (Bool) -> Void) {
+//         appController?.evaluate(inJavaScriptContext: { (context: JSContext) in
+//             let appObject : JSValue = context.objectForKeyedSubscript("App")
+            
+//             if appObject.hasProperty(methodName) {
+//                 appObject.invokeMethod(methodName, withArguments: [])
+//             }
+//             }, completion: completion)
+//     }
+    
     // MARK: Javascript Execution Helper
     
     func executeRemoteMethod(_ methodName: String, completion: @escaping (Bool) -> Void) {
@@ -85,6 +95,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
             // ...
         })
     }
+    
+//     func applicationWillResignActive(_ application: UIApplication) {
+//         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+//         // Use this method to pause ongoing tasks, disable timers, and stop playback
+//         executeRemoteMethod("onWillResignActive", completion: { (success: Bool) in
+//             // ...
+//         })
+//     }
+    
+//     func applicationDidEnterBackground(_ application: UIApplication) {
+//         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+//         executeRemoteMethod("onDidEnterBackground", completion: { (success: Bool) in
+//             // ...
+//         })
+//     }
+    
+//     func applicationWillEnterForeground(_ application: UIApplication) {
+//         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+//         executeRemoteMethod("onWillEnterForeground", completion: { (success: Bool) in
+//             // ...
+//         })
+//     }
+    
+//     func applicationDidBecomeActive(_ application: UIApplication) {
+//         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//         executeRemoteMethod("onDidBecomeActive", completion: { (success: Bool) in
+//             // ...
+//         })
+//     }
     
     // MARK: TVApplicationControllerDelegate
     
